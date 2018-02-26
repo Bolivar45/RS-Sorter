6cd706e49b0cdf2d6e6fd9ff96f8ff1d88a3c10c
Max Drobysh <drobysh.m.s@gmail.com>
2018-02-26 19:04:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.stock = [];
+    this.comtaratorFunc = function (a, b) { return a - b; };
   }
 
   add(element) {
-    // your implementation
+    this.stock.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.stock[index];
   }
 
   get length() {
-    // your implementation
+    return this.stock.length;
   }
 
   toArray() {
-    // your implementation
+    return this.stock;
   }
 
   sort(indices) {
-    // your implementation
+
+    var shortArray = [];
+    indices.sort (
+      function (a, b) { return a - b; }
+    );
+
+    for (let i = 0; i < indices.length; i++) {
+      shortArray.push(this.stock[indices[i]]);
+    }
+
+    shortArray.sort(this.comtaratorFunc);
+
+    for (let i = 0; i < indices.length; i++) {
+      this.stock[indices[i]] = shortArray[i];
+    }
+
+    return this.stock;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comtaratorFunc = compareFunction;
   }
 }
 
